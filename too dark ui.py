a4339be1431c6af311b94065306eb4a7f1745939
Mellow <62353091+FireMarshmallow@users.noreply.github.com>
2021-10-14 06:01:33 +0100

    Broken 14/10/21

--- too dark ui.py
+++ too dark ui.py
@@ -8,24 +8,24 @@ from tkinter.ttk import *
 import tkinter as tk
 
 dark = Tk()
-dark.title('Dark image detector')
+dark.title("Dark image detector")
 
 
 def folder1():
     global folder_selected1
     folder_selected1 = filedialog.askdirectory()
-    Button1['text'] = folder_selected1
+    Button1["text"] = folder_selected1
 
 
-progress = Progressbar(dark, orient=HORIZONTAL, length=250, mode='determinate')
+progress = Progressbar(dark, orient=HORIZONTAL, length=250, mode="determinate")
 
 
 def start_sorting():
     for subdir, dirs, files in os.walk(folder_selected1):
-        if '.DS_Store' in files:
-            files.remove('.DS_Store')
+        if ".DS_Store" in files:
+            files.remove(".DS_Store")
         for file in files:
-            progress['maximum'] = len(files)
+            progress["maximum"] = len(files)
             os.chdir(folder_selected1)
             f = imageio.imread(file)
 
@@ -34,24 +34,34 @@ def start_sorting():
                 if np.mean(img) >= s.get():
                     if not os.path.exists("good"):
                         os.makedirs("good")
-                    shutil.move(file, 'good')
+                    shutil.move(file, "good")
                     # print('good') debuging
                 else:
                     if not os.path.exists("dark"):
                         os.makedirs("dark")
-                    shutil.move(file, 'dark')
+                    shutil.move(file, "dark")
                     # print('bad') debuging
                 progress.step()
+
             dark.update()
             img_estim(f, 127)
 
 
-start = Button(dark, text='Start', command=start_sorting)
-Button1 = tk.Button(dark, text="Select input folder",
-                    activeforeground="blue", command=folder1)
-s = tk.Scale(dark, bg='white', fg='black', label='Set the darkness threshold',
-             from_=0, to=200, orient=HORIZONTAL, length=200,)
-l = Label(dark, width=20, text='empty')
+start = Button(dark, text="Start", command=start_sorting)
+Button1 = tk.Button(
+    dark, text="Select input folder", activeforeground="blue", command=folder1
+)
+s = tk.Scale(
+    dark,
+    bg="white",
+    fg="black",
+    label="Set the darkness threshold",
+    from_=0,
+    to=200,
+    orient=HORIZONTAL,
+    length=200,
+)
+l = Label(dark, width=20, text="empty")
 
 Button1.pack()
 s.pack()
